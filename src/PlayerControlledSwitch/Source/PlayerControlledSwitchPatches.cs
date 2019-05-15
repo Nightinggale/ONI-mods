ef6a857bd66231479d2b04075c6d60d16f6954b7
Nightinggale <Nightinggale.coding@gmail.com>
2019-05-15 20:11:02 +0200

    Added new mod: Player Controlled Switch

---
+++ src/PlayerControlledSwitch/Source/PlayerControlledSwitchPatches.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using NightLib;
+
+namespace PlayerControlledSwitch
+{
+    public static class PlayerControlledSwitch
+    {
+        [HarmonyPatch(typeof(Switch))]
+        [HarmonyPatch("OnMinionToggle")]
+        public static class SwitchTogglePatch
+        {
+            public static bool Prefix(Switch __instance)
+            {
+                ReadPrivate.Call(__instance, "Toggle");
+                return false;
+            }
+        }
+    }
+}
