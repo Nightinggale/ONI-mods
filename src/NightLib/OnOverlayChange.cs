b0af60910b8d167873fa464870a6a1f9efde6d9e
Nightinggale <Nightinggale.coding@gmail.com>
2019-06-09 19:40:48 +0200

    Fix: OnOverlayChange didn't clean up properly when loading a new game

--- src/NightLib/OnOverlayChange.cs
+++ src/NightLib/OnOverlayChange.cs
@@ -14,6 +14,7 @@ namespace NightLib.OnOverlayChange
     internal static class OverlayChangeController
     {
         private static HashSet<IOverlayChangeEvent> list = new HashSet<IOverlayChangeEvent>();
+        private static HashSet<IOverlayChangeEvent> brokenList = new HashSet<IOverlayChangeEvent>();
         private static HashedString lastMode;
 
         internal static void Add(IOverlayChangeEvent obj)
@@ -33,10 +34,33 @@ namespace NightLib.OnOverlayChange
                 HashedString mode = OverlayScreen.Instance.GetMode();
                 if (mode != lastMode)
                 {
+                    bool foundError = false;
+
                     lastMode = mode;
                     foreach (IOverlayChangeEvent obj in list)
                     {
-                        obj.OnOverlayChange(mode);
+                        try
+                        {
+                            obj.OnOverlayChange(mode);
+                        }
+                        catch
+                        {
+                            // loading a game while another game is running might leave garbage.
+                            // the catch condition should only trigger the first time an overlay is changed after load.
+                            brokenList.Add(obj);
+                            foundError = true;
+                        }
+                    }
+                    if (foundError)
+                    {
+                        // Remove all broken buildings.
+                        // Since they were in the last game, they will never recover.
+                        // Removing them will prevent a slowdown from keeping all the garbage.
+                        foreach (IOverlayChangeEvent obj in brokenList)
+                        {
+                            list.Remove(obj);
+                        }
+                        brokenList.Clear();
                     }
                 }
             }
