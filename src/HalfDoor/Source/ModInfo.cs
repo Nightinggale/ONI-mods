727d5cd957a61a630ba6330e092063d466a5e029
Nightinggale <Nightinggale.coding@gmail.com>
2019-06-10 12:12:53 +0200

    Added new mod: Half Door

---
+++ src/HalfDoor/Source/ModInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Nightinggale.HalfDoor
+{
+    public static class ModInfo
+    {
+        public static string Name = "Half Door";
+        public static int Version = 0;
+    }
+}
---
+++ src/HalfDoor/Source/Patches.cs
@@ -0,0 +1,17 @@
+﻿using Harmony;
+
+namespace Nightinggale.HalfDoor
+{
+    public static class BuildingGenerationPatches
+    {
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch("LoadGeneratedBuildings")]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+            public static void Prefix()
+            {
+                Nightinggale.HalfDoor.HalfDoorConfig.Setup();
+            }
+        }
+    }
+}
