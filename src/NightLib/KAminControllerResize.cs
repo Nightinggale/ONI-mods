26bed893f3ecb509f3ed6ca46edc6cffe5b34e80
Nightinggale <Nightinggale.coding@gmail.com>
2019-06-10 12:18:28 +0200

    Added KanimControllerResize

---
+++ src/NightLib/KAminControllerResize.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using STRINGS;
+using UnityEngine;
+
+namespace NightLib
+{
+    [SkipSaveFileSerialization]
+    internal class KAminControllerResize : KMonoBehaviour
+    {
+        public float width = 1f;
+        public float height = 1f;
+
+        [MyCmpGet]
+        private KBatchedAnimController controller;
+
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            if (controller != null)
+            {
+                if (this.width != 1f)
+                {
+                    controller.animWidth = this.width;
+                }
+                if (this.height != 1f)
+                {
+                    controller.animHeight = this.height;
+                }
+            }
+        }
+    }
+
+}
