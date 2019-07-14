e526405291ccc690d1debd3a3526f185d3c9325f
Nightinggale <Nightinggale.coding@gmail.com>
2019-07-14 16:48:53 +0200

    Updated Sensory Overload for Launch

--- src/MoreTemperatureSensors/Source/ConfigFile.cs
+++ src/MoreTemperatureSensors/Source/ConfigFile.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.IO;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace MoreTemperatureSensors
 {
     class MoreTemperatureSensorsConfig
     {
+        private const string fileName = "MoreTemperatureSensorsConfig.json";
+
         public int ItemSensorItemCountFastThreshold = 3;
         public float ItemSensorUpdateIntervalSeconds = 0.15f;
         public float BatterySensorUpdateIntervalSeconds = 0.15f;
@@ -28,22 +31,30 @@ namespace MoreTemperatureSensors
             }
         }
 
-        public static void OnLoad(string modPath)
+        // end of config file variables
+
+        private static MoreTemperatureSensorsConfig internalConfig = null;
+
+        public static MoreTemperatureSensorsConfig Config
         {
-            LoadConfig(modPath);
+            get
+            {
+                if (internalConfig == null)
+                {
+                    // load the config file, but only the first time the method is called.
+                    string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    internalConfig = LoadConfig<MoreTemperatureSensorsConfig>(Path.Combine(assemblyFolder, fileName));
+                }
+                return internalConfig;
+            }
         }
-
-        public static MoreTemperatureSensorsConfig Config = null;
-        public static void LoadConfig(string modPath) { Config = LoadConfig<MoreTemperatureSensorsConfig>(Path.Combine(modPath, "MoreTemperatureSensorsConfig.json")); }
-
-        // modified version of https://github.com/javisar/ONI-Modloader-Mods/blob/Q3-Steam/Source/SpeedControl/SpeedControlConfig.cs
-        // added error handling to make the game work despite missing/malformated json files or missing variables in said files
 
         protected static T LoadConfig<T>(string path) where T : class
         {
             try
             {
                 JsonSerializer serializer = JsonSerializer.CreateDefault(new JsonSerializerSettings { Formatting = Formatting.Indented, ObjectCreationHandling = ObjectCreationHandling.Replace });
+
                 // assign default values in case something isn't mentioned in the file
                 T result = (T)Activator.CreateInstance(typeof(T));
                 using (StreamReader streamReader = new StreamReader(path))
@@ -61,10 +72,9 @@ namespace MoreTemperatureSensors
             {
                 // something went wrong while loading the file, possibly missing or malformated file
                 // return the default values
-                Console.WriteLine("FAILED TO LOAD CONFIG FILE: " + ModInfo.Name);
                 return (T)Activator.CreateInstance(typeof(T));
             }
-            
+
         }
     }
 }
