6c46e38071defcd1333098d9a52daff93079b128
Nightinggale <Nightinggale.coding@gmail.com>
2019-08-31 21:39:18 +0200

    HalfDoor: added version printing to the log at startup

---
+++ src/HalfDoor/Source/WriteNameToLog.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+
+namespace Nightinggale.HalfDoor
+{
+    public static class LogNameWriter
+    {
+        [HarmonyPatch(typeof(GeneratedBuildings))]
+        [HarmonyPatch("LoadGeneratedBuildings")]
+        public static class GeneratedBuildings_LoadGeneratedBuildings_Patch
+        {
+            public static void Prefix()
+            {
+                Console.WriteLine("Loaded mod: " + Assembly.GetExecutingAssembly().GetName());
+            }
+        }
+    }
+}
